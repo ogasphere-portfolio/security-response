e6bb83596736434168fcdfec2eeae862d42d4601
Olivier <olivier@ogasphere.com>
2021-10-27 16:15:50 +0200

    MemberController DONE

--- src/Controller/MemberController.php
+++ src/Controller/MemberController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-use App\Repository\EnterpriseRepository;
+use App\Repository\MemberRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,33 +11,31 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 /**
  * Les annotations de routes au niveau de la classe servent de préfixe à toutes les routes définies dans celle ci
  * 
- * @Route("/entreprises", name="enterprise_")
+ * @Route("/members", name="member_")
 */
-class EnterpriseController extends AbstractController
+class MemberController extends AbstractController
 {
+    
     /**
      * @Route("/", name="list")
      */
     public function list(): Response
     {
-        return $this->render('enterprise/list.html.twig', [
-            'controller_name' => 'EnterpriseController',
+        return $this->render('member/list.html.twig', [
+            'controller_name' => 'MemberController',
         ]);
     }
      /**
      * 
      * @Route("/{id}", name="read")
      */
-    public function read($id,EnterpriseRepository $EnterpriseRepository ): Response
+    public function read($id,MemberRepository $MemberRepository ): Response
     { 
        
-        $enterprise = $EnterpriseRepository->find($id);
+        $member = $MemberRepository->find($id);
       
-        return $this->render('enterprise/read.html.twig', [
-            'enterprise_read' => $enterprise,
+        return $this->render('member/read.html.twig', [
+            'member_read' => $member,
         ]);
     }
-    
-
 }
-
