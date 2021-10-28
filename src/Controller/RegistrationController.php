47042c1e66f50d52889d8bc1f4afe475926c454b
Olivier <olivier@ogasphere.com>
2021-10-28 14:13:50 +0200

    wip

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -48,7 +48,7 @@ class RegistrationController extends AbstractController
             // generate a signed url and email it to the user
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
-                    ->from(new Address('olivier@ogasphere.com', 'Security Response Bot'))
+                    ->from(new Address('secu.response@gmail.com', 'Security Response Bot'))
                     ->to($user->getEmail())
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
