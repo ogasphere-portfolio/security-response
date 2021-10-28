e669404bfa24c5c4523285bde3ed20735dfcd398
MathieuBretaud <mathieu.bretaud@gmail.com>
2021-10-28 17:20:56 +0200

    add fixture roles in user

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -25,6 +25,7 @@ class AppFixtures extends Fixture
             $user = new User();
             $user->setEmail($faker->email())
                 ->setPassword($faker->password())
+                ->setRoles([$faker->word()])
                 ->setCreatedAt(new \DateTimeImmutable())
                 ->setIsVerified(true);
             $manager->persist($user);
@@ -32,6 +33,7 @@ class AppFixtures extends Fixture
             $userList[] = $user;
         }
 
+        $memberList = [];
         for ($i = 0; $i <= 9; $i++) {
             $member = new Member();
             $member->setFirstname($faker->firstName())
@@ -44,20 +46,11 @@ class AppFixtures extends Fixture
                 ->setJobStatus($faker->numberBetween(0, 1))
                 ->setCreatedAt(new \DateTimeImmutable());
             $manager->persist($member);
+
+            $memberList[] = $member;
         }
-
-
-
-        for ($i = 0; $i <= 9; $i++) {
-            $announcement = new Announcement();
-            $announcement->setTitle($faker->sentence())
-                ->setDescription($faker->paragraphs(2, true))
-                ->setStatus($faker->numberBetween(0, 1))
-                ->setCreatedAt(new \DateTimeImmutable());
-
-            $manager->persist($announcement);
-        }
-
+        
+        //$enterpriseList = [];
         for ($i = 0; $i <= 9; $i++) {
             $enterprise = new Enterprise();
             $enterprise->setBusinessName($faker->unique()->enterpriseTitle())
@@ -69,9 +62,30 @@ class AppFixtures extends Fixture
                 ->setCreatedAt(new \DateTimeImmutable());
 
             $manager->persist($enterprise);
+
+            //$enterpriseList[] = $enterprise;
         }
 
 
+        //$announcementList = [];
+        for ($i = 0; $i <= 9; $i++) {
+            $announcement = new Announcement();
+            $announcement->setTitle($faker->sentence())
+                ->setDescription($faker->paragraphs(2, true))
+                ->setStatus($faker->numberBetween(0, 1))
+                ->addMember($memberList[$i])
+                ->setCreatedAt(new \DateTimeImmutable());
+
+            $manager->persist($announcement);
+
+            //$announcementList[] = $announcement;
+        }
+
+       
+
         $manager->flush();
     }
+
+
+
 }
