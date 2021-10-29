1afd776a51e9354cb1eca315fd5500b3e14f2250
MathieuBretaud <mathieu.bretaud@gmail.com>
2021-10-29 14:27:10 +0200

    fixed to string duplicate

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -152,10 +152,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         return $this;
     }
 
-    public function __toString() 
-    {
-        return $this->email;
-    }
 
     public function getCreatedAt(): ?\DateTimeImmutable
     {
