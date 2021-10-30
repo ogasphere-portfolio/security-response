41009f8d5734aa4a735c2cb93dcbddc30e2bfc03
Olivier <olivier@ogasphere.com>
2021-10-30 11:43:44 +0200

    create BackOffice Bread AnnouncementController

---
+++ src/Controller/BackOffice/AnnouncementController.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace App\Controller\BackOffice;
+
+use App\Entity\Announcement;
+use App\Form\AnnouncementType;
+use App\Repository\AnnouncementRepository;
+use DateTimeImmutable;
+use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints\Date;
+
+/**
+ * @Route("/backoffice/announcement", name="backoffice_announcement_")
+ * @IsGranted("IS_AUTHENTICATED_FULLY")
+ */
+class AnnouncementController extends AbstractController
+{
+
+    /**
+     * @Route("/", name="browse", methods={"GET"})
+     */
+    public function browse(AnnouncementRepository $announcementRepository): Response
+    {
+
+        return $this->render('backoffice/announcement/browse.html.twig', [
+            'announcement_browse' => $announcementRepository->findAll()
+        ]);
+    }
+
+    /**
+     * @Route("/read/{id}", name="read", methods={"GET"}, requirements={"id"="\d+"})
+     */
+    public function read(Request $request, Announcement $announcement): Response
+    {
+
+        $announcementForm = $this->createForm(AnnouncementType::class, $announcement, [
+            'disabled' => 'disabled'
+        ]);
+
+        $announcementForm
+            ->add('createdAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('updatedAt', null, [
+                'widget' => 'single_text',
+            ]);
+
+        // on fournit ce formulaire à notre vue
+        return $this->render('backoffice/announcement/read.html.twig', [
+            'form' => $announcementForm->createView(),
+            'announcement' => $announcement,
+        ]);
+    }
+
+    /**
+     * @Route("/edit/{id}", name="edit", methods={"GET", "POST"}, requirements={"id"="\d+"})
+     */
+    public function edit(Request $request, Announcement $announcement): Response
+    {
+        $announcementForm = $this->createForm(AnnouncementType::class, $announcement);
+
+        $announcementForm->handleRequest($request);
+
+        if ($announcementForm->isSubmitted() && $announcementForm->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+
+            $announcement->setUpdatedAt(new DateTimeImmutable());
+            $entityManager->flush();
+
+            $this->addFlash('success', "Announcement `{$announcement->getTitle()}` udpated successfully");
+
+            return $this->redirectToRoute('backoffice_announcement_browse');
+        }
+
+
+        return $this->render('backoffice/announcement/add.html.twig', [
+            'form' => $announcementForm->createView(),
+            'announcement' => $announcement,
+            'page' => 'edit',
+        ]);
+    }
+    /**
+     * @Route("/add", name="add", methods={"GET", "POST"})
+     */
+    public function add(Request $request): Response
+    {
+        $announcement = new Announcement();
+
+
+        $announcementForm = $this->createForm(AnnouncementType::class, $announcement);
+
+
+        $announcementForm->handleRequest($request);
+
+
+        if ($announcementForm->isSubmitted() && $announcementForm->isValid()) {
+
+
+            $entityManager = $this->getDoctrine()->getManager();
+
+            $entityManager->persist($announcement);
+            $entityManager->flush();
+
+
+            $this->addFlash('success', "Announcement `{$announcement->getTitle()}` created successfully");
+
+
+            return $this->redirectToRoute('backoffice_announcement_browse');
+        }
+
+
+        return $this->render('backoffice/announcement/add.html.twig', [
+            'form' => $announcementForm->createView(),
+            'page' => 'create',
+        ]);
+    }
+
+    /**
+     * @Route("/delete/{id}", name="delete", methods={"GET"}, requirements={"id"="\d+"})
+     */
+    public function delete(Announcement $announcement, EntityManagerInterface $entityManager): Response
+    {
+        $this->addFlash('success', "Announcement {$announcement->getId()} deleted");
+
+        $entityManager->remove($announcement);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('backoffice_announcement_browse');
+    }
+}
