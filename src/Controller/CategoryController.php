db05029b339d835514e5e7f67876742bc978883d
Olivier <olivier@ogasphere.com>
2021-10-30 11:08:39 +0200

    create read method in CategoryController

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Category;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CategoryController extends AbstractController
 {
@@ -15,6 +17,33 @@ class CategoryController extends AbstractController
     {
         return $this->render('category/index.html.twig', [
             'controller_name' => 'CategoryController',
+        ]);
+    }
+
+    /**
+     * @Route("/read/{id}", name="read", methods={"GET"}, requirements={"id"="\d+"})
+     */
+    public function read(Request $request, Category $category): Response
+    {
+        // on créé un formulaire avec l'objet récupéré
+        // on modifie dynamiquement (dans le controleur) les options du formulaire
+        // pour désactiver tous les champs
+        $categoryForm = $this->createForm(CategoryType::class, $category, [
+            'disabled' => 'disabled'
+        ]);
+
+        $categoryForm
+            ->add('createdAt', null, [
+            'widget' => 'single_text',
+        ])
+            ->add('updatedAt', null, [
+            'widget' => 'single_text',
+        ]);
+
+        // on fournit ce formulaire à notre vue
+        return $this->render('backoffice/category/read.html.twig', [
+            'form' => $categoryForm->createView(),
+            'category' => $category,
         ]);
     }
 }
