68b57cb01084a72bc7f233798d521f53c22c09c6
Olivier <olivier@ogasphere.com>
2021-11-01 12:32:02 +0100

    WIP intégration AdminLte

---
+++ src/Controller/BackOffice/MainController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller\BackOffice;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+/**
+ * @Route("/backoffice", name="backoffice_")
+ * @IsGranted("IS_AUTHENTICATED_FULLY")
+ */
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function browse(): Response
+    {
+        return $this->render('backoffice/home_admin.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+}
