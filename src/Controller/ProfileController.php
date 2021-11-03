c9fddde324b1112807ecef5c0a91e3232534e967
Guerric <sehnotech@gmx.fr>
2021-11-03 15:41:33 +0100

    Routing profil enterprise debug

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,19 +2,43 @@
 
 namespace App\Controller;
 
+use App\Entity\Enterprise;
+use App\Entity\Member;
+use App\Repository\EnterpriseRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/profil", name="profile_")
+ */
 class ProfileController extends AbstractController
 {
     /**
-     * @Route("/profile", name="profile")
+     * @Route("/entreprise/{id}", name="enterprise", methods={"GET"})
      */
-    public function index(): Response
+    public function enterpriseHome(Request $request, EnterpriseRepository $enterpriseRepository): Response
     {
-        return $this->render('profile/index.html.twig', [
+        
+        return $this->render('profile/enterprise/home.html.twig', [
+            'enterprise' => $enterpriseRepository,
+        ]);
+    }
+
+    /**
+     * @Route("/member/{id}", name="member")
+     */
+    public function memberHome(Request $request, Member $member): Response
+    {
+        $memberForm = $this->createForm(MemberType::class, $member);
+
+        $memberForm->handleRequest($request);
+
+        return $this->render('profile/member/home.html.twig', [
             'controller_name' => 'ProfileController',
+            'form' => $memberForm->createView(),
+               
         ]);
     }
 }
