4feb3e79d9cb97273ced881d5ba37743f11f627e
Sylvain SAINTE ROMANE <sylvain.sainteromane@gmail.com>
2021-11-15 10:15:23 +0100

    add list member in nav bar DONE

--- src/Controller/MemberController.php
+++ src/Controller/MemberController.php
@@ -188,7 +188,7 @@ class MemberController extends AbstractController
 
         return $this->render('member/read.html.twig', [
             'member_read' => $member,
-        ]);
+        ]);        
     }
 
 
