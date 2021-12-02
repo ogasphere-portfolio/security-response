783d9b0a20cb071ab89a4e55879ad33909f455ab
Sylvain SAINTE ROMANE <sylvain.sainteromane@gmail.com>
2021-12-02 12:46:14 +0100

    password visibility DONE

--- src/Controller/BackOffice/CompanyController.php
+++ src/Controller/BackOffice/CompanyController.php
@@ -6,16 +6,22 @@ use App\Repository\CompanyRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
+/**
+ * @Route("/backoffice/company", name="backoffice_company_")
+ * @IsGranted("IS_AUTHENTICATED_FULLY")
+ */
 class CompanyController extends AbstractController
 {
     /**
-     * @Route("/company", name="company")
+     * @Route("/", name="browse", methods={"GET"})
      */
     public function index(CompanyRepository $companyRepository): Response
     {
-        return $this->render('company/index.html.twig', [
-            'controller_name' => 'CompanyController',
+        return $this->render('backoffice/company/browse.html.twig', [
+            'company_list' => $companyRepository->findAll()
+
         ]);
     }
 }
